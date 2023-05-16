16afb25f7f876880dfbe5b68b2a60e20b23c6ce0
Victor Daussy-Renaudin <victor@precisionlife.com>
2023-05-16 10:30:56 +0100

    Removes swap files and add vim-fugitive

--- plugins/user.lua
+++ plugins/user.lua
@@ -22,14 +22,26 @@ return {
       }
     end,
   },
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "tpope/vim-fugitive",
+    lazy = false
+  },
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = function()
+      require('neogen').setup {
+        enabled = true,
+        languages = {
+          python = {
+            template = {
+              annotation_convention =
+              "reST"
+            }
+          }
+        },
+      }
+    end
+  }
 }
